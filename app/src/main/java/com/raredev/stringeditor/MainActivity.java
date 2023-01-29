f9b998966d5a977be231ddd15133287fbe4d5917
raredeveloperofc <raredev123e@gmail.com>
2023-01-29 14:09:58 -0300

    .

--- app/src/main/java/com/raredev/stringeditor/MainActivity.java
+++ app/src/main/java/com/raredev/stringeditor/MainActivity.java
@@ -15,7 +15,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import com.blankj.utilcode.util.ToastUtils;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.google.android.material.textfield.TextInputEditText;
-import com.itsaky.androidide.logsender.LogSender;
 import com.raredev.stringeditor.adapter.StringsAdapter;
 import com.raredev.stringeditor.databinding.ActivityMainBinding;
 import com.raredev.stringeditor.utils.SourceUtils;
@@ -39,7 +38,6 @@ public class MainActivity extends AppCompatActivity {
   }
 
   private void init() {
-    LogSender.startLogging(this);
     setSupportActionBar(binding.toolbar);
     adapter = new StringsAdapter(listString);
 
