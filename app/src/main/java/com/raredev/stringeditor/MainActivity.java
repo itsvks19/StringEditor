4bc28195f71d939c3a2e60bdba4a577bef42ca5a
Vivek Kumar Sahani <itsvks19@gmail.com>
2023-01-30 19:18:23 +0530

    Remove LogSender

--- app/src/main/java/com/raredev/stringeditor/MainActivity.java
+++ app/src/main/java/com/raredev/stringeditor/MainActivity.java
@@ -13,7 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.blankj.utilcode.util.ToastUtils;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
-import com.itsaky.androidide.logsender.LogSender;
+// import com.itsaky.androidide.logsender.LogSender;
 import com.raredev.stringeditor.adapter.StringsAdapter;
 import com.raredev.stringeditor.callback.DialogCallback;
 import com.raredev.stringeditor.callback.ItemMoveCallBack;
